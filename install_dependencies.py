d1b9f7974d96c0cd9f05cd52b2946c1b14aa0cff
bigjoedata <blechman@gmail.com>
2019-04-18 14:23:28 -0400

    Create install_dependencies.py

---
+++ install_dependencies.py
@@ -0,0 +1,29 @@
+#Install as needed. Not all guaranteed to work or be compatible.
+!python -m pip install --upgrade PyHamcrest
+!python -m pip install --upgrade jupyter_contrib_nbextensions
+!python -m pip install --upgrade selenium
+!python -m pip install --upgrade tweepy
+!python -m pip install --upgrade nltk
+!python -m pip install --upgrade twython
+!python -m pip install --upgrade textblob
+!python -m pip install --upgrade sklearn
+!python -m pip install --upgrade numpy
+!python -m pip install --upgrade matplotlib
+!python -m pip install --upgrade seaborn
+!python -m pip install --upgrade vaderSentiment
+!python -m pip install --upgrade pyDrive
+!python -m pip install --upgrade jsonpickle
+!python -m pip install tweetfeels
+#don't work? !python -m pip install --upgrade threading, gcc, urllib2, openssl-devel, urllib, python-devel python-setuptools 
+#!python -m pip install --upgrade collections
+!python -m pip install --upgrade datetime
+!python3 -m nltk.downloader vader_lexicon
+!python -m pip install --upgrade google-api-python-client
+!python -m pip install --upgrade gsutil
+!python -m pip install --upgrade libffi-devel
+!python -m pip install --upgrade google-auth google-auth-oauthlib google-auth-httplib2
+!python -m pip install --upgrade requests
+!python -m pip install --upgrade dfareporting_utils
+!python -m pip install --upgrade twython
+!python -m pip install --upgrade youtube-python
+!python -m pip install --upgrade google-ads
